efe180b06df5397e5043f858eff1b2ab6d6dfe54
vbrzezinska <varya.bzhezinskaya@gmail.com>
2021-04-14 09:40:55 +0300

    code improvements

--- app/src/main/java/com/example/mynetdiarytest/ui/BaseListItemDelegate.kt
+++ app/src/main/java/com/example/mynetdiarytest/ui/BaseListItemDelegate.kt
@@ -1,7 +1,6 @@
 package com.example.mynetdiarytest.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import com.hannesdorfmann.adapterdelegates4.AbsListItemAdapterDelegate
@@ -14,10 +13,4 @@ abstract class BaseListItemDelegate<T : ListItem>(
 
     override fun onCreateViewHolder(parent: ViewGroup): ListItemViewHolder =
         ListItemViewHolder(LayoutInflater.from(parent.context).inflate(layoutId, parent, false))
-
-    fun getInitializedView(item: T, parent: ViewGroup): View {
-        val viewHolder = this.onCreateViewHolder(parent)
-        this.onBindViewHolder(item, viewHolder, mutableListOf())
-        return viewHolder.itemView
-    }
 }
